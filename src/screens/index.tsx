068ab586c1abba297e9a2df7f3c5f51354559043
ottogugel <harpgamer31@gmail.com>
2023-12-19 00:28:56 -0300

    Aula 02 - Fundamentos

---
+++ src/screens/index.tsx
@@ -0,0 +1,67 @@
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native'
+import { styles } from './styles'
+import { Participant } from '../components/Participant'
+
+export function Home() {
+
+  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Leo', 'Kaio', 'Ana', 'Bia', 'Lucy'];
+
+  function handleParticipantAdd() {
+    if(participants.includes("Rodrigo")){
+      return Alert.alert("Participante Existe", "Já existe um participante na lista com esse nome")
+    }
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover", `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert("Deletado!")
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Novo evento</Text>
+
+      <Text style={styles.eventDate}>Segunda, 18 de dezembro de 2023.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
+    </View>
+  );
+}
+
